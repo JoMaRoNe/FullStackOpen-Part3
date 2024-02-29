9eaaed090c4f396e8374a27d8f60b7d3edf75fec
JoMaRoNe <jmromero@duck.com>
2024-02-29 22:31:01 +0100

    Exercise 3.2 finished

--- index.js
+++ index.js
@@ -29,8 +29,16 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const people = persons.length
+    const date = new Date().toString()
+
+    response.send(`<p>Phonebook has info for ${people} people</p><p>${date}</p>`)
+}) 
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
     console.log(`http://localhost:${PORT}/api/persons`)
+    console.log(`http://localhost:${PORT}/info`)
 })
