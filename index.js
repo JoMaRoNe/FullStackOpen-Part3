2478b39fcec81ea5d46968a3feb187646881824b
JoMaRoNe <jmromero@duck.com>
2024-03-01 21:34:52 +0100

    Exercise 3.7 finished

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 
@@ -72,11 +74,6 @@ app.post('/api/persons', (request, response) => {
 
     response.json(person)
   }
-
-  
-
-
-
 })
 
 
