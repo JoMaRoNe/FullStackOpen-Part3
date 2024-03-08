bf120b3a3d69129bad6a5b780213db397371feec
JoMaRoNe <jmromero@duck.com>
2024-03-08 17:26:47 +0100

    Exercise 3.9 finished

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
-
-//app.use(morgan('tiny'))
+app.use(cors())
 
 morgan.token('body', (request) => {
   return JSON.stringify(request.body)
@@ -91,9 +92,16 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${people} people</p><p>${date}</p>`)
 }) 
 
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+
+/*
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
     console.log(`http://localhost:${PORT}/api/persons`)
     console.log(`http://localhost:${PORT}/info`)
 })
+*/
