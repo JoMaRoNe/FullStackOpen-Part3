15020395150954e835ce4a8da1a06df7b1065f6f
JoMaRoNe <jmromero@duck.com>
2024-03-09 10:53:36 +0100

    Exercise 3.14 finished

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/Person')
 
 const app = express()
 
@@ -14,31 +16,10 @@ morgan.token('body', (request) => {
 
 app.use(morgan(':method :url :status :response-time :body')) 
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
     response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -52,37 +33,30 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  console.log(body)
+  
+  if ( body.name === '' || body.number === '' ) {
+    return response.status(400).json({error:'Name and/or Number is missing'})
+    } 
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+  
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
+
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(p => p.id !== id)
 
   response.status(204).end()
 })
-
-app.post('/api/persons', (request, response) => {
-  const body = request.body
-  
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error:'Name and/or Number is missing'
-    })
-  } else if (persons.find(p => p.name === body.name)) {
-      return response.status(400).json({
-        error: 'name must be unique'
-      })
-  } else {
-    const person = {
-      id: Math.floor(Math.random() * 300),
-      name: body.name,
-      number: body.number
-    }
-
-    persons = persons.concat(person)
-
-    response.json(person)
-  }
-})
-
 
 app.get('/info', (request, response) => {
     const people = persons.length
