11ee4f20d36f61a5a0ae1d8308ed3d556f8cf038
JoMaRoNe <127348963+JoMaRoNe@users.noreply.github.com>
2024-03-14 19:39:31 +0100

    Delete FrontEnd-phonebook directory

--- FrontEnd-phonebook/src/components/Person.jsx
+++
@@ -1,7 +0,0 @@
-const Person = ({person, handle}) => {
-    return (
-        <p>{person.name} {person.number} {person.id} <button onClick={() =>handle(person.name, person.id)}>delete</button></p>
-    )
-}
-
-export default Person
